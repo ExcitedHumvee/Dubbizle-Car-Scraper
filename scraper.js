bef938eecbc460a9f53737da15f4a5dc8cef0ebd
Stanislaus Desa <35200248+ExcitedHumvee@users.noreply.github.com>
2025-09-16 16:14:26 +0400

    saving to DB

--- scraper.js
+++ scraper.js
@@ -6,9 +6,9 @@ const path = require('path'); // Path module for creating file paths
 // Configuration
 const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/';
 const FIRST_PAGE = 1;
-const LAST_PAGE = 200; // do not go greater than 400
+const LAST_PAGE = 4; // do not go greater than 400
 const SAVE_HTML_PAGES = false;
-const CONCURRENT_PAGES = 10;
+const CONCURRENT_PAGES = 1;
 const PAGE_TIMEOUT = 10; // seconds we wait for page to load
 
 if (SAVE_HTML_PAGES === false) {
