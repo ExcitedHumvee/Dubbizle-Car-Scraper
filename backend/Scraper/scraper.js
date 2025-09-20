9acf51ff6f392dc4a37a14925cb1fb3fe773deac
ExcitedHumvee <35200248+ExcitedHumvee@users.noreply.github.com>
2025-09-20 16:12:59 +0400

    ch

--- backend/Scraper/scraper.js
+++ backend/Scraper/scraper.js
@@ -6,9 +6,9 @@ const path = require('path'); // Path module for creating file paths
 // Configuration
 const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/';
 const FIRST_PAGE = 1;
-const LAST_PAGE = 10; // do not go greater than 400
+const LAST_PAGE = 100; // do not go greater than 400
 const SAVE_HTML_PAGES = false;
-const CONCURRENT_PAGES = 5; // increasing this may lead to to more errors, __NEST_DATA__ wont load properly for many pages
+const CONCURRENT_PAGES = 1; // increasing this may lead to to more errors, __NEST_DATA__ wont load properly for many pages
 const TIMEOUT = 10; // seconds
 
 if (SAVE_HTML_PAGES === false) {
