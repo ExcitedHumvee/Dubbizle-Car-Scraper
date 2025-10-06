34c611159151b23c56da5852565521a3488d4884
Stanislaus Desa <35200248+ExcitedHumvee@users.noreply.github.com>
2025-10-06 10:47:30 +0400

    up

--- backend/Scraper/scraper.js
+++ backend/Scraper/scraper.js
@@ -4,13 +4,13 @@ const fs = require('fs'); // File System module for writing files
 const path = require('path'); // Path module for creating file paths 
 
 // Configuration
-// const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_date=dsc'; // dsc means newest first
-const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=desc'; // asc means lowest price first
+const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_date=dsc'; // dsc means newest first
+// const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=desc'; // asc means lowest price first
 const FIRST_PAGE = 1;
 const LAST_PAGE = 400; // do not go greater than 400
 const SAVE_HTML_PAGES = false;
 const CONCURRENT_PAGES = 1; // increasing this may lead to to more errors, __NEST_DATA__ wont load properly for many pages
-const TIMEOUT = 30; // seconds
+const TIMEOUT = 10; // seconds
 
 if (SAVE_HTML_PAGES === false) {
     console.warn('WARNING: SAVE_HTML_PAGES is set to false. Raw HTML pages will not be saved.');
