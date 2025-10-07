64103c9794182221f6e2481e17c89447480ce790
Stanislaus Desa <35200248+ExcitedHumvee@users.noreply.github.com>
2025-10-07 12:38:25 +0400

    up

--- backend/Scraper/scraper.js
+++ backend/Scraper/scraper.js
@@ -4,7 +4,7 @@ const fs = require('fs'); // File System module for writing files
 const path = require('path'); // Path module for creating file paths 
 
 // Configuration
-const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_date=dsc'; // dsc means newest first
+const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/'; // dsc means newest first
 // const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=desc'; // asc means lowest price first
 const FIRST_PAGE = 1;
 const LAST_PAGE = 400; // do not go greater than 400
@@ -41,7 +41,7 @@ if (!fs.existsSync(processedDir)) {
 }
 
 async function scrapePage(browser, pageNum) {
-    const url = `${BASE_URL}&page=${pageNum}`;
+    const url = `${BASE_URL}?page=${pageNum}`;
     console.log(`Navigating to: ${url}`);
     let page;
     try {
