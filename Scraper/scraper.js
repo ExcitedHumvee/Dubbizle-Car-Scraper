3963737ef4dceff8d00202adb2a1f98a0300e4b5
Stanislaus Desa <35200248+ExcitedHumvee@users.noreply.github.com>
2025-10-08 12:36:51 +0400

    up

--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -7,7 +7,7 @@ const path = require('path'); // Path module for creating file paths
 const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/'; // dsc means newest first
 // const BASE_URL = 'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=desc'; // asc means lowest price first
 const FIRST_PAGE = 1;
-const LAST_PAGE = 3; // do not go greater than 400
+const LAST_PAGE = 400; // do not go greater than 400
 const SAVE_HTML_PAGES = false;
 const CONCURRENT_PAGES = 1; // increasing this may lead to to more errors, __NEST_DATA__ wont load properly for many pages
 const TIMEOUT = 10; // seconds
