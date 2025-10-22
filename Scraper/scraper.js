7c339bcbe369ae5f443f536c9d47e391596d903f
Stanislaus Desa <35200248+ExcitedHumvee@users.noreply.github.com>
2025-10-22 16:05:11 +0400

    up

--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -6,10 +6,10 @@ const path = require('path'); // Path module for creating file paths
 // Configuration
 // The scraper will iterate through each of these base URLs sequentially.
 const BASE_URLS = [
-    // 'https://dubai.dubizzle.com/motors/used-cars/',
+    'https://dubai.dubizzle.com/motors/used-cars/',
     'https://dubai.dubizzle.com/motors/used-cars/?sorting=date_desc',
-    // 'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=desc',
-    // 'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=asc'
+    'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=desc',
+    'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=asc'
 ];
 const FIRST_PAGE = 1;
 const LAST_PAGE = 400; // do not go greater than 400
