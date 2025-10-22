8fff5346dc2db481be2715601d992fd0b578d523
Stanislaus Desa <35200248+ExcitedHumvee@users.noreply.github.com>
2025-10-22 16:19:00 +0400

    up

--- Scraper/scraper.js
+++ Scraper/scraper.js
@@ -12,7 +12,7 @@ const BASE_URLS = [
     'https://dubai.dubizzle.com/motors/used-cars/?sorting_price=asc'
 ];
 const FIRST_PAGE = 1;
-const LAST_PAGE = 10; // do not go greater than 400
+const LAST_PAGE = 100; // do not go greater than 400
 const SAVE_HTML_PAGES = false;
 const CONCURRENT_PAGES = 1; // increasing this may lead to to more errors, __NEST_DATA__ wont load properly for many pages
 const TIMEOUT = 10; // seconds
